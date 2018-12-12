0e8bdd44682f3203de9274e43176b94370474402
BryanPiek03 <45109007+BryanPiek03@users.noreply.github.com>
2018-12-12 16:33:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const req    = require('snekfetch');
+const Eris   = require('eris');
+const client = new Eris('NTIwMjY4Njk4Njc4OTg0NzQ5.DvFy6w.y3sO-Xb5oX5WE8r9p0UuTwpAydQ');
+
+client.connect();
+
+client.on('ready', async () => {
+    const vc = await client.joinVoiceChannel('522071201128906752');
+    vc.play('http://stream.radiocorp.nl/web11_aac');
+});
+
+
+
+client.on('messageCreate', msg => {
+    if (msg.author.bot) return;
+
+    if (msg.content === 'slam save') 
+        msg.author.createMessage(now);
+    if (msg.content === 'slam stats') 
+        msg.channel.createMessage({ embed: {
+            color: 0xE60268,
+            title: 'SLAM! Stats',
+            description: `Ping: ${msg.channel.guild.shard.latency}ms\n` +
+                `RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n` +
+                `Uptime: ${process.uptime() / 60} minutes`
+        }});
+
+    if (msg.content === 'slam np') 
+        msg.channel.createMessage({ embed: {
+            color: 0xE60268,
+            title: 'Now Playing',
+            description: now
+        }});
+
+    if (msg.content === 'slam help') 
+        msg.channel.createMessage('slam < save | stats | np >');
+});
+
+let now = '';
+
+setInterval(async () => {
+    let res = await req.get('https://live.slam.nl/slam-hardstyle/metadata/hardstyle_livewall').set('User-Agent', 'discord-slambot/1.0');
+    if (res.status !== 200 || !res.body.nowArtist || !res.body.nowTitle || now === `${res.body.nowArtist} - ${res.body.nowTitle}`) return;
+
+    now = `${res.body.nowArtist} - ${res.body.nowTitle}`;
+    client.editStatus({ name: `${res.body.nowArtist} - ${res.body.nowTitle}` });
+}, 10000)
